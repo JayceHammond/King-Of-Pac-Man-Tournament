ab877c07b88a584ac2a8f47972b6e4355d9796b8
JayceHammond <hjayce6@gmail.com>
2023-10-26 09:00:11 -0500

    Cell class + grid drawing

--- mapGenerator.py
+++ mapGenerator.py
@@ -1,5 +1,6 @@
 import pygame as p
 import numpy
+from random import choice
 
 # Define some colors
 BLACK = (0, 0, 0)
@@ -9,9 +10,69 @@ RED = (255, 0, 0)
 ORANGE = (255, 165, 0)
 YELLOW = (255, 255, 0)
 CYAN = (0, 255, 255)
-MAGENTA = (255, 0, 255)
-BLUE = (0, 0, 255)
+FROYALBLUE = (79, 132, 166)
 
+
+res = width, height = 1202, 723
+tile = 48
+cols, rows = width // tile, height // tile
+
+class Cell:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.walls = {'top': True, 'right': True, 'bottom': True, 'left': True}
+        self.visited = False
+
+    def draw(self):
+        x, y = self.x * tile, self.y * tile
+        
+        if self.visited:
+            p.draw.rect(sc, p.Color(BLACK), (x,y, tile, tile))
+
+        if self.walls['top']:
+            p.draw.line(sc, ORANGE, (x,y), (x + tile, y), 2)
+        if self.walls['right']:
+            p.draw.line(sc, ORANGE, (x + tile, y), (x + tile, y + tile), 2)
+        if self.walls['bottom']:
+            p.draw.line(sc, ORANGE, (x + tile, y + tile), (x, y + tile), 2)
+        if self.walls['left']:
+            p.draw.line(sc, ORANGE, (x, y + tile), (x, y), 2)
+
+    def drawCurrentCell(self):
+        x = self.x * tile
+        y = self.y * tile
+        p.draw.rect(sc, FROYALBLUE, (x + 2, y + 2, tile - 2, tile - 2))
+
+    def checkCell(self, x, y):
+        findIndex = lambda x, y: x + y * cols
+        if x < 0 or x > cols - 1 or y < 0 or y < rows - 1:
+            return False
+        return gridCells[findIndex(x,y)]
+    
+    def check_neighbors(self):
+        neighbors = []
+        top = self.checkCell(self.x, self.y - 1)
+        right = self.checkCell(self.x + 1, self.y)
+        left = self.checkCell(self.x - 1, self.y)
+        bottom = self.checkCell(self.x, self.y + 1)
+
+        if top and not top.visited:
+            neighbors.append(top)
+        if right and not right.visited:
+            neighbors.append(right)
+        if left and not left.visited:
+            neighbors.append(left)
+        if bottom and not bottom.visited:
+            neighbors.append(bottom)
+
+        return choice(neighbors) if neighbors else False
+
+
+gridCells = [Cell(col, row) for row in range(rows) for col in range(cols)]
+currCell = gridCells[0]
+stack = []
+        
 
 tileSheet = "Pac-Man Assets\Tiles\MazeParts.png"
 
@@ -21,15 +82,26 @@ def get_pixels_at(x, y, width, height, map):
     pixels.blit(map, (0,0), rect)
     return pixels
 
+
 p.init()
-clock = p.time.Clock()
-screen_width = 720
-screen_height = 720
-screen = p.display.set_mode([screen_width, screen_height])
-get_pixels_at(0,0, 48, 48, tileSheet)
-running = True
-while running:
+sc = p.display.set_mode(res)
+clock =p.time.Clock()
+
+while True:
+    sc.fill(p.Color(BLACK))
+
     for event in p.event.get():
         if event.type == p.QUIT:
-            running = False
-        
+            exit()
+
+    [cell.draw() for cell in gridCells]
+    currCell.visited = True
+    currCell.drawCurrentCell()
+
+    nextCell = currCell.check_neighbors()
+    if True:
+        nextCell.visited = True
+        currCell = nextCell
+
+    p.display.flip()
+    clock.tick(60)
